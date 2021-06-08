5faf75d7eb7265875ad60f1d4c9edf0e90f82046
roma_front <prog3@gerchikco.com>
2021-06-08 14:11:26 +0300

    chnage script

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
     const slider = document.querySelector(".slider");
     const beforeImg = document.querySelector(".before");
-    const afterImg = document.querySelector(".after");
-
-    const setSlide = x => {
-        beforeImg.style.width = `${x}px`;
-    }
 
     let isActive = false;
 
+    const slide = x => {
+        if (!isActive) return; 
+        let shift = Math.max(0, Math.min(x, slider.offsetWidth));
+        beforeImg.style.width = `${ shift }px`;
+    }
+
     const getOffsetX = e => {
         return e?.offsetX;
-    }
+    };
 
     slider.addEventListener('mousedown', e => {
         isActive = true;
@@ -20,20 +21,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
     slider.addEventListener('mouseup', () => {
         isActive = false;
-    })
-
-    const slide = x => {
-        if (!isActive) return; 
-        let shift = Math.max(0, Math.min(x, slider.offsetWidth));
-        setSlide(shift);
-    }
+    });
 
     slider.addEventListener("mouseleave", () => {
         isActive = false;
-    })
+    });
 
     
     slider.addEventListener('mousemove', e => {
         slide(getOffsetX(e))
-    })
+    });
 })
